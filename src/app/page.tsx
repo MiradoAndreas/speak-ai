adf91ce14f88ddb3017b25c55fef699faf454104
Mirado <vonjiniainamirado@gmail.com>
2026-04-28 15:19:21 +0300

    03: Add basic authentification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,53 @@
+"use client"
+
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { authClient } from '@/lib/auth-client'
+import { useState } from 'react'
+import { toast } from 'sonner'
 
 const Page = () => {
+
+  const { data: session } = authClient.useSession()
+
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const onSubmit = () => {
+    authClient.signUp.email({
+      email,
+      name,
+      password
+    }, {
+      onError: () => {
+        toast.error("Someting went wrong")
+      },
+      onSuccess: () => {
+        toast.success(`Bienvenu ${name}`)
+      }
+    })
+  }
+
+  if (session) {
+    return (
+      <div className='flex flex-col p-4 gap-y-4'>
+        <p>Logged in as {session.user.name}</p>
+        <Button onClick={() => authClient.signOut()}>
+          Sign out
+        </Button>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      <Button>
-        Hello world
+    <div className='flex flex-col gap-y-4 min-w-xl md:min-w-2xl mx-auto my-2'>
+      <Input placeholder='name' value={name} onChange={(e) => setName(e.target.value)} />
+      <Input placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+      <Input placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+      <Button onClick={onSubmit}>
+        Create User
       </Button>
     </div>
   )
