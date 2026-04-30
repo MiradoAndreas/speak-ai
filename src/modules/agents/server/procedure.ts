e5990babe282e9bf167a1b4c8b8bdf226a9e2744
Mirado <vonjiniainamirado@gmail.com>
2026-04-30 20:28:52 +0300

    09: Agents Setup

---
+++ src/modules/agents/server/procedure.ts
@@ -0,0 +1,12 @@
+import { agents } from "@/db/schema";
+import { db } from "@/index";
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { TRPCError } from "@trpc/server";
+
+export const agentRouter = createTRPCRouter({
+  getMany: baseProcedure.query(async () => {
+    const data = await db.select().from(agents);
+
+    return data;
+  }),
+});
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,26 +1,13 @@
 "use client"
 
-import { ClientGreeting } from "@/app/(dashboard)/client-greeting"
-import { Button } from "@/components/ui/button"
-import { authClient } from "@/lib/auth-client"
-import { useRouter } from "next/navigation"
+
 
 
 export const HomeView = () => {
-  const router = useRouter()
-  const {
-    data: session
-  } = authClient.useSession()
-  if (!session) {
-    return (
-      <p>
-        Loading...
-      </p>
-    )
-  }
+
   return (
-    <div className="p-4">
-      <ClientGreeting />
+    <div>
+      Home view
     </div>
   )
 }
